2665f9cd9cf1be9c90675363e6d39d1d9173cd3e
Adam Watkins <adam@stupidpupil.co.uk>
2021-09-27 18:04:05 +0100

    Incorporate CTax

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-This repository attempts to provide figures on the number of HMOs in each Cardiff ward versus the total number of dwellings.
+This repository attempts to provide figures on the number of HMOs in each Cardiff ward versus the total number of dwellings, as of 
 
 ## Beware
 
-- Licencing requirements for HMOs are more strict in Cathays and Plasnewydd, and so many more properties are registered as HMOs in these wards
+- Licencing requirements for HMOs are more strict in Cathays and Plasnewydd, and so many more properties are licensed as HMOs in these wards
 
 - Households resident in HMOs would be better metric than the proportion of properties
 
@@ -13,7 +13,7 @@ The repository contains:
 - Dwelling stock information from the VOA provided under the OGLv3
 
 It also contains data derived from:
-- HMO licence information made [available online by Cardiff Council "for non-commercial purposes"](http://ishare.cardiff.gov.uk/mycardiff.aspx?ms=Cardiff_Live/AllMaps&layers=hmoinfo%2cctax&starteasting=318835.5&startnorthing=177777.69989014&startzoom=7770), but also inspectable on the Council's HMO register at their headquarters
+- Council tax and HMO licence information made [available online by Cardiff Council "for non-commercial purposes"](http://ishare.cardiff.gov.uk/mycardiff.aspx?ms=Cardiff_Live/AllMaps&layers=hmoinfo%2cctax&starteasting=318835.5&startnorthing=177777.69989014&startzoom=7770), but also inspectable on the Council's HMO register at their headquarters
 
 ## How to
 ```R
--- link_hmos_to_lsoas.R
+++ link_hmos_to_lsoas.R
@@ -1,15 +1,22 @@
 library(sf)
 library(tidyverse)
 
-hmos <- st_read("data-raw/licences.geojson")
+licences <- st_read("data-raw/licences.geojson")
+ctax <- st_read("data-raw/ctax.geojson")
 lsoas <- st_read("data-raw/lsoa11_boundaries_full.geojson") %>%
   rename(LSOA11CD = LSOA11Code)
 
-hmos_by_lsoa <- hmos %>% 
+licences_by_lsoa <- hmos %>% 
   st_join(lsoas) %>% 
   st_drop_geometry() %>% 
   group_by(LSOA11CD) %>%
   summarise(CountLicensedHMOs = n())
+
+ctax_by_lsoa <- ctax %>% 
+  st_join(lsoas) %>% 
+  st_drop_geometry() %>% 
+  group_by(LSOA11CD) %>%
+  summarise(CountCTaxHMOs = n())
 
 dwellings_by_lsoa <- read_csv("data-raw/Table_CTSOP4_1_2020.csv") %>%
   filter(band == "All") %>%
@@ -19,16 +26,18 @@ dwellings_by_lsoa <- read_csv("data-raw/Table_CTSOP4_1_2020.csv") %>%
   ) %>%
   select(LSOA11CD, CountDwellings)
 
-
 dwellings_by_lsoa %>% 
   left_join(read_csv("data-raw/Lower_Layer_Super_Output_Area__2011__to_Ward__2020__Lookup_in_England_and_Wales.csv")) %>%
-  left_join(hmos_by_lsoa) %>%
+  left_join(licences_by_lsoa) %>%
+  left_join(ctax_by_lsoa) %>%
   filter(LAD20NM %>% str_detect("Cardiff")) %>%
   group_by(WD20NM) %>%
   summarise(
     CountLicensedHMOs = sum(CountLicensedHMOs, na.rm=TRUE),
+    CountCTaxHMOs = sum(CountCTaxHMOs, na.rm=TRUE),
     CountDwellings = sum(CountDwellings),
-    PropHMOs = CountLicensedHMOs/CountDwellings
+    PropLicensedHMOs = CountLicensedHMOs/CountDwellings,
+    PropCTaxHMOs = CountCTaxHMOs/CountDwellings
   ) %>% 
-  arrange(-PropHMOs) %>%
+  arrange(-PropCTaxHMOs) %>%
   write_excel_csv("output/hmos_by_ward_2019.csv")
